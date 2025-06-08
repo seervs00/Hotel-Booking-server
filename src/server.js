0e30584db5589f35e01ad9ab8fc379a65012be4f
mahendra <mahendras00b@gmail.com>
2025-06-08 21:48:35 +0530

    setup

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import "dotenv/config";
 import cors from 'cors'
 import {clerkMiddleware} from '@clerk/express'
-import clerkWebhooks from "./src/controllers/clerkWebhooks.js";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 const app = express();
 
 app.use(express.json())
