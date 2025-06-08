37b880c70b87abba64fe48fac6d34e2312b3f1b5
mahendra <mahendras00b@gmail.com>
2025-06-08 21:14:00 +0530

    setup

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import "dotenv/config";
+import cors from 'cors'
+import {clerkMiddleware} from '@clerk/express'
+import clerkWebhooks from "./src/controllers/clerkWebhooks.js";
+const app = express();
+
+app.use(express.json())
+app.use(clerkMiddleware())
+app.use(cors());
+
+// app.post('/user',async(req,res) => {
+//     const {name,email,password,role} = req.body
+
+//     const NewUser =  await prisma.user.create({
+//         data:{
+//             name:name,
+//             email:email,
+//             password:password,
+//             role:role
+//         }
+//     })
+//     res.send(NewUser)
+// })
+app.use("/api/clerk" , clerkWebhooks)
+app.get('/',(req,res) => res.send("hello doston"));
+
+const PORT = process.env.PORT||5000
+
+app.listen(PORT , () => console.log(`server listen ${PORT} Sccessfully`))
---
+++ src/controllers/clerkWebhooks.js
@@ -0,0 +1,58 @@
+import prisma from "../configs/db.config.js"
+import { Webhook } from "svix";
+ 
+const clerkWebhooks = async(req,res) => {
+    try{
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        
+        const headers ={
+            "svix-id":req.headers['svix-id'],
+            'svix-timestamp':req.headers['svix-timestamp'],
+            'svix-signature':req.headers['svix-signature'],     
+          }
+
+       await whook.verify(JSON.stringify(req.body),headers);
+       
+       const {data ,type} = req.body
+       
+       const userData = {
+        _id:data.id,
+        email:data.email_addresses[0].email_address,
+        username: data.first_name + " " + data.last_name,
+        image:data.image_url,
+       }
+
+       switch(type){
+        case "user.created":{
+            await prisma.user.create({data:userData})
+            break;
+        }
+        case "user.updated":{
+      await prisma.user.update({
+           where:{_id:data.id},
+           data:userData
+            })
+         break;
+
+        }
+        case "user.deleted":{
+            await prisma.user.delete({
+                where:{_id:data.id}
+            })
+            break;
+        }
+        default:
+            break;
+       }
+       res.json({success:true ,message:"webhook recieved"})
+
+    }
+
+    catch(error){
+        console.log(error.message)
+        res.json({success: false , message:error.message})
+     
+    }
+}
+
+export default clerkWebhooks; 
