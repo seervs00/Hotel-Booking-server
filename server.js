6821afe6ee8aa59b5a782174597f7edd33d5ec87
mahendra <mahendras00b@gmail.com>
2025-06-09 09:31:28 +0530

    d

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-import express from "express"
-import "dotenv/config";
-import cors from 'cors'
-import {clerkMiddleware} from '@clerk/express'
-import clerkWebhooks from "./src/controllers/clerkWebhooks.js";
+// import express from "express"
+// import "dotenv/config";
+// import cors from 'cors'
+// import {clerkMiddleware} from '@clerk/express'
+// import clerkWebhooks from "./src/controllers/clerkWebhooks.js";
+// const app = express();
+
+// app.use(express.json())
+// // app.use(clerkMiddleware())
+// app.use(cors());
+
+
+
+// // app.use("/api/clerk" , clerkWebhooks)
+// app.get('/',(req,res) => res.send("hello doston"));
+
+// // const PORT = process.env.PORT||3000
+
+// // app.listen(PORT , () => console.log(`server listen ${PORT} Sccessfully`))
+// export default app;
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-app.use(express.json())
-// app.use(clerkMiddleware())
 app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel backend!");
+});
 
-
-// app.use("/api/clerk" , clerkWebhooks)
-app.get('/',(req,res) => res.send("hello doston"));
-
-// const PORT = process.env.PORT||3000
-
-// app.listen(PORT , () => console.log(`server listen ${PORT} Sccessfully`))
+// 👇 IMPORTANT: export, don’t listen
 export default app;
